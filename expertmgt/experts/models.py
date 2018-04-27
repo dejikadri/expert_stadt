5825e940d1a52896728572d95d7383382c53b881
Deji Kadri <adedeji.kadri@andela.com>
2018-04-27 14:09:54 +0100

    created the Expert model

--- expertmgt/experts/models.py
+++ expertmgt/experts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Expert(models.Model):
+    """
+    This Model allows contains the definition for Experts
+
+    """
+   full_name =  models.CharField(max_length=150)
+   email_address = models.CharField(max_length=100)
+   short_description =  models.CharField(max_length=200)
+   full_description =  models.TextField()
+   popularity =  models.IntegerField()
+   path_to_picture =  models.CharField(max_length=150)
+   hourly_rate =  models.FloatField()
+   total_hours_taught =  models.IntegerField()
+
+
+    def __str__(self):
+        """
+        Unicode representation of Experts.
+        """
+        return self.
