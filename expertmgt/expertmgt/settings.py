e3abaf842f790c4c27bcf2fb8c0ca31c800f3206
Deji Kadri <adedeji.kadri@andela.com>
2018-05-02 07:07:43 +0100

    Ran migrations

--- expertmgt/expertmgt/settings.py
+++ expertmgt/expertmgt/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'experts.apps.ExpertConfig'
+    'experts.apps.ExpertsConfig'
 ]
 
 MIDDLEWARE = [
