b8a2ba4ae8e41a1255998802182fdfcdb2792b5b
Deji Kadri <adedeji.kadri@andela.com>
2018-05-02 07:03:07 +0100

    added app to setting.py, registered model in admin.py and fixed model indentation issue

--- expertmgt/experts/models.py
+++ expertmgt/experts/models.py
@@ -6,21 +6,21 @@ class Expert(models.Model):
     This Model allows contains the definition for Experts
 
     """
-   first_name =  models.CharField(max_length=150)
-   last_name =  models.CharField(max_length=150)
-   email_address = models.CharField(max_length=100)
-   short_description =  models.CharField(max_length=200)
-   full_description =  models.TextField()
-   university = models.CharField(max_length=250)
-   major = models.CharField(max_length=250)
-   popularity =  models.IntegerField()
-   path_to_picture =  models.CharField(max_length=150)
-   hourly_rate =  models.FloatField()
-   total_hours_taught =  models.IntegerField()
+    first_name =  models.CharField(max_length=150)
+    last_name =  models.CharField(max_length=150)
+    email_address = models.CharField(max_length=100)
+    short_description =  models.CharField(max_length=200)
+    full_description =  models.TextField()
+    university = models.CharField(max_length=250)
+    major = models.CharField(max_length=250)
+    popularity =  models.IntegerField()
+    path_to_picture =  models.CharField(max_length=150)
+    hourly_rate =  models.FloatField()
+    total_hours_taught =  models.IntegerField()
 
 
     def __str__(self):
         """
         Unicode representation of Experts.
         """
-        return "{} {} ()".format(self.first_name, self.last_name, short_description) 
+        return "{} {} ()".format(self.first_name, self.last_name, self.short_description) 
