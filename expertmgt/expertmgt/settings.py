45d785037fc1f7167fabc3e3e64676a8cc8e66c6
Deji Kadri <adedeji.kadri@andela.com>
2018-05-15 14:28:10 +0100

    Resolved CORS issue with react in seetings.py

--- expertmgt/expertmgt/settings.py
+++ expertmgt/expertmgt/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'experts.apps.ExpertsConfig'
+    'experts.apps.ExpertsConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'expertmgt.urls'
 
